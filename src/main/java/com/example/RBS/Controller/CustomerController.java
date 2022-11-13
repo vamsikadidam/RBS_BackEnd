54372833f28e3abef1f72cb297bcff5001a6611b
vamsikadidam <vamsi.kadidam@gmail.com>
2022-11-13 15:20:43 +0530

    added database

--- src/main/java/com/example/RBS/Controller/CustomerController.java
+++ src/main/java/com/example/RBS/Controller/CustomerController.java
@@ -1,32 +1,65 @@
 package com.example.RBS.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
-import java.util.stream.Collectors; 
+import java.util.stream.Collectors;
 
+import com.example.RBS.Entity.Loan;
+import com.example.RBS.Repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.RBS.Entity.Customer;
 import com.example.RBS.Repository.CustomerRepository;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="http://localhost:4200/")
 public class CustomerController {
-	
-//	@Autowired
-//	private CustomerRepository customerRepository;
-	
-	@GetMapping("customer")
-	public List<Customer> getCustomers(){
-		
-		return Stream.of(new Customer("12345", "vk","address","ABCDE1234F","String@1"),
-				new Customer("12346", "vk1","addressArea","ABCDE1234G","String@1"),
-				new Customer("12347", "vk2","addressPlace","ABCDE1234H","String@1")).collect(Collectors.toList());
-				
-//				this.customerRepository.findAll();
+
+	@Autowired
+	LoanRepository loanRepo;
+
+
+	@GetMapping("/getCustomer")
+	public List<Loan> getCustomers() {
+
+		Customer customer = new Customer();
+		customer.customerId = 1;
+		customer.customerName = "Vamsi";
+		customer.address = "Hyderabad";
+		customer.pan = "ABCD123";
+
+		Loan loan1 = new Loan();
+		loan1.loanId = 1;
+		loan1.loanAmount = 100000;
+		loan1.loanType = "Education";
+		loan1.customer=customer;
+
+
+		Loan loan2 = new Loan();
+		loan2.loanId = 2;
+		loan2.loanAmount = 200000;
+		loan2.loanType = "Agriculture";
+		loan2.customer=customer;
+
+
+
+		List<Loan> list = new ArrayList<>();
+
+		list.add(loan1);
+		list.add(loan2);
+
+        return loanRepo.saveAll(list);
+	}
+
+	@GetMapping("loandetails/{id}")
+	public Loan getLoanDetails(@PathVariable int id){
+		return loanRepo.findById(id).get();
+
 	}
 
 }
--- src/main/java/com/example/RBS/Entity/Customer.java
+++ src/main/java/com/example/RBS/Entity/Customer.java
@@ -1,209 +1,27 @@
 package com.example.RBS.Entity;
 
-import java.util.Date;
 
-import javax.persistence.Column;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
 
 @Entity
-//@NoArgsConstructor
-//@Getter
-//@Setter
-@Table(name="Customer")
 public class Customer {
 
-	/**
-	 * Customer Entity persisted in Repository
-	 */
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name = "slno")
-//	private long id;
-//	
-//	private String loanType;
-//	private int durationOfMonths;
-//	private float roi;
-//	
-//	
-//
-//	public Customer(String loanType, int durationOfMonths, float roi) {
-//		super();
-//		this.loanType = loanType;
-//		this.durationOfMonths = durationOfMonths;
-//		this.roi = roi;
-//	}
-//
-//
-//	public String getLoanType() {
-//		return loanType;
-//	}
-//
-//	public void setLoanType(String loanType) {
-//		this.loanType = loanType;
-//	}
-//
-//	public int getDurationOfMonths() {
-//		return durationOfMonths;
-//	}
-//
-//	public void setDurationOfMonths(int durationOfMonths) {
-//		this.durationOfMonths = durationOfMonths;
-//	}
-//
-//	public float getRoi() {
-//		return roi;
-//	}
-//
-//	public void setRoi(float roi) {
-//		this.roi = roi;
-//	}
-
-	
-	
-
-//	public Customer() {
-//		super();
-//		// TODO Auto-generated constructor stub
-//	}
-
-	public Customer(//long id,
-			String customerId,
-//			double loanAmount,
-			@NotBlank @Size(min = 3, max = 20, message = "Customer Name should be in between 3 to 20 characters") String customerName,
-			@NotBlank(message = "Address should not be blank") @Size(max = 50) String address, 
-//			Date dateOfBirth,
-			@NotBlank(message = "PAN number should not be blank") @Pattern(regexp = "[A-Z]{5}[0-9]{4}[A-Z]{1}", message = "Pan No should contain 5 Capital letters, 4 digits, 1 Capital letter") String panNo,
-			
-			@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}", message = "It contains atleast 8 characters and atmost 20 characters, atleast one digit, atleast one uppercase alphabet, atleast one lowercase alphabet, atleast one special character which includes !@#$%&*()-+=^., doesn't contain any whitespace.") String password) {
-		super();
-//		this.id = id;
-		this.customerId = customerId;
-		this.customerName = customerName;
-		this.address = address;
-//		this.dateOfBirth = dateOfBirth;
-		this.panNo = panNo;
-		this.password = password;
-	}
-
-//	public long getId() {
-//		return id;
-//	}
-//
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-
-
-	public String getCustomerId() {
-		return customerId;
-	}
-
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
-	}
-
-	public String getCustomerName() {
-		return customerName;
-	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-//	public Date getDateOfBirth() {
-//		return dateOfBirth;
-//	}
-//
-//	public void setDateOfBirth(Date dateOfBirth) {
-//		this.dateOfBirth = dateOfBirth;
-//	}
-
-	public String getPanNo() {
-		return panNo;
-	}
-
-	public void setPanNo(String panNo) {
-		this.panNo = panNo;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "customerId")
-	private String customerId;
+	public int customerId;
 
-	@NotBlank
-	@Size(min = 3, max = 20, message = "Customer Name should be in between 3 to 20 characters")
-	private String customerName;
+	public String customerName;
 
-	@NotBlank(message = "Address should not be blank")
-	@Size(max = 50)
-	private String address;
+	public String address;
 
-//	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-//	private Date dateOfBirth;
+	public String pan;
 
-	@NotBlank(message = "PAN number should not be blank")
-	@Pattern(regexp = "[A-Z]{5}[0-9]{4}[A-Z]{1}", message = "Pan No should contain 5 Capital letters, 4 digits, 1 Capital letter")
-	private String panNo;
-	
-//	private double loanAmount;
-//
-//	
-//
-//	public double getLoanAmount() {
-//		return loanAmount;
-//	}
-//
-//	public void setLoanAmount(double loanAmount) {
-//		this.loanAmount = loanAmount;
-//	}
-
-	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}", message = "It contains atleast 8 characters and atmost 20 characters, atleast one digit, atleast one uppercase alphabet, atleast one lowercase alphabet, atleast one special character which includes !@#$%&*()-+=^., doesn't contain any whitespace.")
-	private String password;
-
-//	@Override
-//	public String toString() {
-//		return "Customer [id=" + id + ", customerId=" + customerId + ", customerName=" + customerName + ", address="
-//				+ address + ", dateOfBirth=" + dateOfBirth + ", panNo=" + panNo + ", loanAmount=" + loanAmount
-//				+ ", password=" + password + ", loanType=" + loanType + ", durationOfMonths=" + durationOfMonths + ", roi=" + roi + "]";
-//	}
 
 }
 
-
-
-//public class Customer {
-//
-//}
